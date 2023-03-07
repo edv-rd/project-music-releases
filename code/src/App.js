fbba8765ad3fee5e2c02c86b9eb23e8a6b20d503
Ed Code <edvard.ekstrom@gmail.com>
2023-03-07 19:39:44 +0100

    fix to merge

--- code/src/App.js
+++ code/src/App.js
@@ -9,6 +9,7 @@ export const App = () => {
       <hr />
       <div className="albums-container">
         {data.albums.items.map((items) => {
+          // console.log(items)
           return (
             <Album key={items.id} album={items} />
           )
