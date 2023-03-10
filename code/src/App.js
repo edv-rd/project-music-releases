359532bad04123687a611cbc4e8a9abc62d97696
Ed Code <edvard.ekstrom@gmail.com>
2023-03-10 14:46:38 +0100

    album and singles section

--- code/src/App.js
+++ code/src/App.js
@@ -5,10 +5,19 @@ import { Album } from './components/Album'
 export const App = () => {
   return (
     <div className="main-container">
-      <h1>New albums & singles</h1>
+      <h1>New albums</h1>
       <hr />
       <div className="albums-container">
-        {data.albums.items.map((items) => {
+        {data.albums.items.filter((item) => item.album_type === 'album').map((items) => {
+          return (
+            <Album key={items.id} album={items} />
+          )
+        })}
+      </div>
+      <h1>New singles</h1>
+      <hr />
+      <div className="albums-container">
+        {data.albums.items.filter((item) => item.album_type === 'single').map((items) => {
           return (
             <Album key={items.id} album={items} />
           )
