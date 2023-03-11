6d2242398c9ad30f7dfc4cc94b26753669edc27a
Ed Code <edvard.ekstrom@gmail.com>
2023-03-11 15:31:23 +0100

    styling and responsitivity

--- code/src/components/Playlist.js
+++ code/src/components/Playlist.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 export const Playlist = (props) => {
   return (
-    <div className="playlist-button">
-      <a key={props.playlist.id} href={props.playlist.external_urls.spotify} className="playlist-name">{props.playlist.name}</a>
+    <div className="playlist-card">
+      <a
+        key={props.playlist.id}
+        href={props.playlist.external_urls.spotify}
+        className="playlist-link"><img className="playlist-image" src={props.playlist.images[0].url} alt={props.playlist.description} />
+        <div className="playlist-text">
+          <span className="playlist-name">{props.playlist.name}</span>
+          <p className="playlist-description">{props.playlist.description}</p>
+        </div>
+      </a>
     </div>
   )
 }
--- code/src/components/Playlists.js
+++ code/src/components/Playlists.js
@@ -3,14 +3,16 @@ import { Playlist } from './Playlist'
 
 export const Playlists = (props) => {
   return (
-    <div className="playlists">
+    <>
       <h1>Playlists</h1>
       <hr />
-      {props.data.playlists.items.map((playlist) => {
-        return (
-          <Playlist playlist={playlist} />
-        )
-      })}
-    </div>
+      <div className="playlists">
+        {props.data.playlists.items.map((playlist) => {
+          return (
+            <Playlist playlist={playlist} />
+          )
+        })}
+      </div>
+    </>
   )
 }
